4f24780c94cad21b5b7860aef6a155c896f8e44b
Maria Eduarda Lima <meduardalima88@gmail.com>
2023-09-20 03:00:45 -0300

    update integracao back e front

--- backEnd/src/index.js
+++ backEnd/src/index.js
@@ -2,12 +2,14 @@ const express = require('express');
 
 const rotas = require('./rotas');
 
+const path = require('path');
+
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'frontEnd')));
 
 app.use(express.json());
 
 app.use(rotas);
 
-app.listen(3000);
+app.listen(5000);
